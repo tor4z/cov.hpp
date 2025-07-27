c1b5198cf72f842e23175cfcf8d406242c8068ad
tor4z <vwenjie@hotmail.com>
2025-07-27 13:00:31 +0800

    Define custom matrix in shader

--- examples/03-multi_layer_matmul.cpp
+++ examples/03-multi_layer_matmul.cpp
@@ -9,7 +9,8 @@ int main()
 {
     // suppose we run this program on build dir
     // const std::string shader_path{"../examples/shader/multi_layer_matmul.comp.spv"};
-    const std::string shader_path{"../examples/shader/multi_layer_matmul2.comp.spv"};
+    // const std::string shader_path{"../examples/shader/multi_layer_matmul2.comp.spv"};
+    const std::string shader_path{"../examples/shader/multi_layer_matmul3.comp.spv"};
 
     Mat A{2, 2};
     Mat B{2, 2};
